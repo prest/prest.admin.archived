874416bd0bd280a588838c2f108d98c0a3861b29
HigorSegobia <higor.segobia@gmail.com>
2020-09-29 09:48:17 -0300

    Remove return and correct CamelCase

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import ErrorPage from '~/components/PageError';
+import PageError from '~/components/PageError';
 import BackgroundParticles from '~/components/BackgroundParticles';
 
-export const pageNotFound = (): React.ReactElement => {
-  return (
-    <>
-      <ErrorPage
-        code="404"
-        message="page not found"
-        description="We are sorry but the page are looking for does not exist."
-      />
-      <BackgroundParticles />
-    </>
-  );
-};
+export const PageNotFound = (): React.ReactElement => (
+  <>
+    <PageError
+      code="404"
+      message="page not found"
+      description="We are sorry but the page are looking for does not exist."
+    />
+    <BackgroundParticles />
+  </>
+);
 
-export default pageNotFound;
+export default PageNotFound;
