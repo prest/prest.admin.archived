132dae24c3f2213339df904276387f4d119f9ffe
HigorSegobia <higor.segobia@gmail.com>
2020-10-14 16:05:00 -0300

    Refactoring of error page tests

--- tests/components/PageError.spec.tsx
+++ tests/components/PageError.spec.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import PageError from '../../src/components/PageError';
+import { PageError } from '../../src/components/PageError';
 
 describe('components/PageError', () => {
   it('should render component properly', () => {
     const fakeCode = '404';
     const fakeDescription = 'bar';
     const fakeMessage = 'foo';
-    const wrap = shallow(<PageError code={fakeCode} message={fakeMessage} description={fakeDescription} />);
-
-    expect(wrap).toHaveProp('code', fakeCode);
-    expect(wrap).toHaveProp('message', fakeMessage);
-    expect(wrap).toHaveProp('description', fakeDescription);
-    expect(wrap).toBeCalled;
+    const wrap = shallow(
+      <PageError classes={{}} code={fakeCode} message={fakeMessage} description={fakeDescription} />,
+    );
+    console.log(wrap.debug());
+    expect(wrap.find('h1')).toHaveText(`${fakeCode} ${fakeMessage}`);
+    expect(wrap.find('h2')).toHaveText(fakeDescription);
   });
 });
